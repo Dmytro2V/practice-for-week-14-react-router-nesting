de1593cf2f30eb5cceecbc3e2b2986c9b3a63c03
Dmytro2V <op2vin@gmail.com>
2023-03-25 03:45:37 +0000

    done all Update index.js and index.js

--- src/components/MovieDetails/index.js
+++ src/components/MovieDetails/index.js
@@ -1,7 +1,13 @@
-function MovieDetails() {
+import {useParams} from 'react-router-dom';
+function MovieDetails({movies}) {
+  const {movieId} = useParams();
+  console.log(movies, 'moId', movieId, movieId === String(movies[0].id)) ;
+  let movieChoice = movies.find(mov => String(mov.id) === movieId )
+  console.log('moCho', movieChoice);
   return (
     <div className='comp purple'>
-      <h1>MovieDetails Component</h1>
+      <h1>{movieChoice.title}</h1>
+      <p>{movieChoice.description}</p>
     </div>
   );
 }
